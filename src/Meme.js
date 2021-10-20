2ba9fcf7f6b8d76fb249d2047621ee7314485b1d
Christen McCool <christenmccool@Christens-MacBook-Pro.local>
2021-10-20 16:13:25 -0700

    create delete div rather than button

--- src/Meme.js
+++ src/Meme.js
@@ -11,9 +11,10 @@ const Meme = ({id, url, top, bottom}) => {
   return (
     <div className="Meme">
       <img className="Meme-img" src={url} alt="meme" />
-      <button className="Meme-btn" onClick={handleClick}>X</button>
+      {/* <button className="Meme-btn" onClick={handleClick}>X</button>  */}
       <div className="Meme-top-caption">{top}</div>
       <div className="Meme-bottom-caption">{bottom}</div>
+      <div className="Meme-remove-div" onClick={handleClick}>X</div>
     </div>
   )
 }
